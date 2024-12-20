64d016c88d4bd60652608a352a83e37260e9825b
riteshhsolankii <riteshhsolankii@gmail.com>
2024-12-20 16:23:54 +0530

    changes new

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,19 @@
+import { CiShoppingCart } from "react-icons/ci";
+
 function Sidebar() {
+  const students = [1, 2, 3, 4];
   return (
     <div className="sidebar-main">
       <div className="sidebar-inner">
-        <img src="/images/logo.png" alt="Logo" />
+        <img src="/logo.png" alt="Logo" />
+        <p>Dashboard</p>
+        <div className="sidebar-url">
+          <ul>
+            {students.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
