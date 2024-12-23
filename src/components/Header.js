dc2103f06cebfadb8575ea2fe6c7ba99eaeeef6a
riteshhsolankii <riteshhsolankii@gmail.com>
2024-12-23 15:16:40 +0530

    23/12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from "react";
-import logo from "/logo.png";
-
-const 
 
 function Header() {
   return (
     <header className="header">
-      <div className="header-main">
-        <div className="logo-main">
-          <a href="/" aria-label="Go to homepage">
-            <img src={logo} alt="Company Logo" />
-          </a>
+      <div className="container">
+        <div className="header-main">
+          <div className="header-logo"></div>
+          <div className="header-nav">
+            <nav>
+              <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
