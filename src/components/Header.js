61573ffd8ef266c04291f08c8f51b5abcb6260cd
riteshhsolankii <riteshhsolankii@gmail.com>
2024-12-23 17:37:33 +0530

    today final changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
-import React from "react";
-
 function Header() {
   return (
-    <header className="header">
+    <header className="header-main bg-black">
       <div className="container">
-        <div className="header-main">
-          <div className="header-logo"></div>
-          <div className="header-nav">
-            <nav>
-              <ul>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-              </ul>
-            </nav>
+        <div className="header-inner-main py-4 px-4 flex align-middle justify-between">
+          <div className="logo">
+            <img src="/logo.png" />
           </div>
+          <nav>
+            <ul className="flex space-x-10">
+              <li className="text-white">Home</li>
+              <li className="text-white">About Us</li>
+              <li className="text-white">Servies</li>
+              <li className="text-white">Contact Us</li>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
